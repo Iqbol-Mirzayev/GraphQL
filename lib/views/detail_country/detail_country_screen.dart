99f78d50848150efe1c5f7b479c09282d9cfb955
Iqbol-Mirzayev <sfafsasf@gmail.com>
2022-10-19 10:03:30 +0500

    view started

---
+++ lib/views/detail_country/detail_country_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class DetailCountryScreen extends StatelessWidget {
+  const DetailCountryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
